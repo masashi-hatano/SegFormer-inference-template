ee7564e2e2ae23d8fc0e20b3e5334e3a63798a0b
H570-PLUS\masashi <hatano153@gmail.com>
2023-02-03 15:27:13 +0900

    action confirmation

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -8,12 +8,14 @@ class Dataset(torch.utils.data.Dataset):
     def __init__(self, data_dir):
         super(Dataset, self).__init__()
         self.data_dir = Path(data_dir)
-        self.path_to_imgs = self.data_dir.glob("**/*.png")
+        self.path_to_imgs = list(self.data_dir.glob("**/*.png"))
 
     def __getitem__(self, index):
         # load images
-        img = cv2.imread(self.path_to_imgs[index]).astype(np.float32)
+        img = cv2.imread(str(self.path_to_imgs[index])).astype(np.float32)
         img = torch.from_numpy(img)
+        # [H, W, C] -> [C, H, W]
+        img = img.permute(2, 0, 1)
         file_name = str(self.path_to_imgs[index].name)
         return img, file_name
 
