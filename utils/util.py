e004a88616d222712452472db490f8c15ac05b8a
H570-PLUS\masashi <hatano153@gmail.com>
2023-02-03 15:27:41 +0900

    delete debug

--- utils/util.py
+++ utils/util.py
@@ -11,7 +11,6 @@ def convertToRGB(index_colored_numpy, palette, n_colors=19):
     return np.dot(expanded_img, use_palette).astype(np.uint8)
 
 def paletteToRGB(pred, size, palette):
-    print(pred.shape)
     pred = F.interpolate(input=pred, size=size, mode='bilinear', align_corners=False)
     pred = np.asarray(np.argmax(pred.cpu(), axis=1), dtype=np.uint8)
     pred_rgb = convertToRGB(pred, palette)
